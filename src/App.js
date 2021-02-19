c07038a4e2c89c4476caf73b58ad54785a6c5bdf
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-02-19 22:47:05 +0000

    Working calculator

--- src/App.js
+++ src/App.js
@@ -1,78 +1,154 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Slider from '@material-ui/core/Slider';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
-let api = "https://openfisca-uk.herokuapp.com"
+const api = "https://openfisca-uk.herokuapp.com";
+const cors = "https://cors-escape.herokuapp.com/";
 
 function App() {
+  const root = {
+    flexGrow: 1,
+  }
   return (
-    <div>
-      <h1>Household Net Income Calculator</h1>
-      <Main />
+    <div style={root}>
+      <AppBar position="static" style={{marginBottom: 20}}>
+        <Toolbar>
+          <Typography variant="h6">
+            UK Tax-Benefit Calculator
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <VariableGrid />
+      </Container>
     </div>
+    
   )
 }
 
-class Main extends React.Component {
-    constructor() {
-      super()
-      this.state = {salary: 0, result: "Computing"};
-      this.handleChange = this.handleChange.bind(this);
-      this.updateHNet = this.updateHNet.bind(this);
-    }
-
-    handleChange(event, newValue) {
-      let timeout;
-      timeout && clearTimeout(timeout);
-      timeout = setTimeout(() => {
-        this.setState({salary: newValue});
-        this.updateHNet();
-      }, 100);
-    }
-
-    updateHNet() {
-      let data = {
-        people: {
-          person1: {
-            age: {2020: 18},
-            earnings: {2020: this.state.salary}
-          }
-        },
-        benunits: {
-          benunit: {
-            adults: ["person1"]
-          }
-        },
-        households: {
-          household: {
-            adults: ["person1"],
-            household_net_income: {2020: null}
-          }
+class VariableGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    let situation_data = {
+      people: {
+        person: {
+          age: {2020: null},
+          earnings: {2020: null},
+          profit: {2020: null},
+          rental_income: {2020: null},
+          state_pension: {2020: null},
+          income_tax: {2020: null},
+          NI: {2020: null},
+          taxable_income: {2020: null},
+          taxable_income_deductions: {2020: null},
+          total_tax: {2020: null}
+        }
+      },
+      benunits: {
+        benunit: {
+          adults: ["person"]
+        }
+      },
+      households: {
+        household: {
+          adults: ["person"],
+          household_net_income: {2020: null},
         }
       }
-      fetch(api + "/calculate", {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      }).then(result => result.json()).then(data => this.setState({result: data.households.household.household_net_income[2020]}))
-
     }
+    let calculated_data = Object.assign({}, situation_data);
+    this.state = {
+      situation_data: situation_data,
+      calculated_data: calculated_data
+    };
+    this.updatePersonField = this.updatePersonField.bind(this);
+    this.updateBenunitField = this.updateBenunitField.bind(this);
+    this.updateHouseholdField = this.updateHouseholdField.bind(this);
+    this.updateData = this.updateData.bind(this);
+  }
 
-    componentDidMount() {
-      this.updateHNet();
-    }
+  updateData() {
+    fetch(api + "/calculate", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state.situation_data)
+    }).then(res => res.json()).then(data => this.setState({calculated_data: data}));
+  }
 
-    render() {
-      return <div>
-        <Slider min={0} max={100000} step={100} onChange={this.handleChange}/>
-        <h3>Your earnings: {this.state.salary}, your household net income: {this.state.result}</h3>
+  componentDidMount() {
+    this.updateData();
+  }
+
+  updatePersonField(event, newKey, newValue) {
+    let data = this.state.situation_data;
+    data.people.person[newKey] = {2020: +newValue};
+    this.setState({situation_data: data});
+    this.updateData();
+  } 
+
+  updateBenunitField(event, newKey, newValue) {
+    let data = this.state.situation_data;
+    data.benunits.benunit[newKey] = {2020: +newValue};
+    this.setState({situation_data: data});
+  } 
+
+  updateHouseholdField(event, newKey, newValue) {
+    let data = this.state.situation_data;
+    data.households.household[newKey] = {2020: newValue ? +newValue : null};
+    this.setState({situation_data: data ? data : null});
+  } 
+
+  render() {
+    const getFields = obj => Object.entries(obj).map(x => (x[0] !== "adults") ? <Variable key={x[0]} name={x[0]} value={Object.entries(x[1])[0][1]} changeHandler={(e) => this.updatePersonField(e, x[0], e.target.value)} /> : null);
+    return (
+      <div>
+        <h2>Person</h2>
+        <Grid container spacing={3}>
+          {getFields(this.state.calculated_data.people.person)}
+        </Grid>
+        <h2>Benunit</h2>
+        <Grid container spacing={3}>
+          {getFields(this.state.calculated_data.benunits.benunit)}
+        </Grid>
+        <h2>Household</h2>
+        <Grid container spacing={3}>
+          {getFields(this.state.calculated_data.households.household)}
+        </Grid>
       </div>
-      
-    }
+    )
+  }
 }
 
+class Variable extends React.Component {
+
+  render() {
+    const style = {
+      minWidth: 100,
+      margin: 10,
+      padding: 10,
+      textAlign: 'center',
+      color: "black",
+    }
+    return (
+      <Paper style={style}>
+        <h3>{this.props.name}</h3>
+        <TextField label={this.props.value} onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            this.props.changeHandler(e)
+            e.preventDefault();
+          }
+        }} />
+      </Paper>
+    )
+  }
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfrh4iVG3HJGS6UZToeA8Vct3uSjLi4kE",
+  authDomain: "openfisca-uk-ui.firebaseapp.com",
+  projectId: "openfisca-uk-ui",
+  storageBucket: "openfisca-uk-ui.appspot.com",
+  messagingSenderId: "809049110771",
+  appId: "1:809049110771:web:f938d29aa98fbebfe85f86",
+  measurementId: "G-DWRJCHKKPH"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
